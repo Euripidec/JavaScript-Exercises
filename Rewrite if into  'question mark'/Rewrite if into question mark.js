33e41f5ca9b44a340ce0147d97c9d3d1934e8912
Euripide Carpio <euripidec@gmail.com>
2023-03-28 21:36:58 -0400

    changes and exercises

---
+++ Rewrite if into  'question mark'/Rewrite if into question mark.js
@@ -0,0 +1,13 @@
+//  code to rewrite: 
+
+//  let result;
+
+// if (a + b < 4) {
+//   result = 'Below';
+// } else {
+//   result = 'Over';
+// }
+
+'use strict';
+
+let result = (a + b < 4) ? "Below" : "Over";
---
+++ Show the Sign/show the sign.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let num = prompt ("Input number", "");
+
+if (num > 0) {
+	alert(1)
+} else if (num < 0) {
+	alert(-1)
+} else  {
+	alert(0)
+}
